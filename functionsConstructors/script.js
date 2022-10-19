bbf1ce19dfc5ea2c858ba7ec9761d7c0549a1790
spandin <willstesi@gmail.com>
2022-10-19 14:06:49 +0300

    Функции конструкторы
    Конструкция обьектов из функции

---
+++ functionsConstructors/script.js
@@ -0,0 +1,19 @@
+1.
+function Car(name, speed) {
+    this.name = name
+    this.speed = speed
+}
+
+const auto1 = new Car('Audi', '80 км/ч')
+
+console.log(auto1);
+
+2.
+function Product(name, book) {
+    this.name = name
+    this.book = book
+}
+
+const book = new Product('Sapiens', '3 - заказа')
+
+console.log(book);
